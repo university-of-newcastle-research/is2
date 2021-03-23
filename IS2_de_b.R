c12398564cc37019c2749327bfd02925450182a4
Gavin Cooper <phd@gavincooper.net>
2021-03-23 22:09:37 +1100

    styler run on scripts using ALEFix

--- IS2_de_b.R
+++ IS2_de_b.R
@@ -1,7 +1,7 @@
 #######       IS2 Psuedo Code       #########
 
-## set up environment and packages 
-rm(list=ls())
+## set up environment and packages
+rm(list = ls())
 library(mixtools)
 library(mvtnorm)
 library(MCMCpack)
@@ -13,77 +13,77 @@ library(msm)
 devtools::load_all()
 
 load("b_fit.RData")
-burnin=round(nmc/2)
-cpus = 1
+burnin <- round(nmc / 2)
+cpus <- 1
 ###### set up variables #####
 # number of particles, samples, subjects, random effects etc
-n_randeffect=length(theta[1,,1,1])
-n_subjects = length(theta[1,1,,1])
-n_iter = length(theta[1,1,1,])- burnin
-#length_draws = sampled$samples$idx #length of the full transformed random effect vector and/or parameter vector
-IS_samples = 100 #number of importance samples
-n_particles = 100 #number of particles
-par.names = names(theta[1,,1,1])
-hpar.names = names(phi[1,,1])
+n_randeffect <- length(theta[1, , 1, 1])
+n_subjects <- length(theta[1, 1, , 1])
+n_iter <- length(theta[1, 1, 1, ]) - burnin
+# length_draws = sampled$samples$idx #length of the full transformed random effect vector and/or parameter vector
+IS_samples <- 100 # number of importance samples
+n_particles <- 100 # number of particles
+par.names <- names(theta[1, , 1, 1])
+hpar.names <- names(phi[1, , 1])
 
 
 # grab the sampled stage of PMwG
 # store the random effects - theta
-theta_samples <- theta[,,,burnin:5000]
-theta_samples <- aperm(theta_samples, c(1,4,2,3))
-dim(theta_samples)<- c((2501*20),5,40)
-theta_samples <- theta_samples[seq(1, length(theta_samples[,1,1]), 10),,]
+theta_samples <- theta[, , , burnin:5000]
+theta_samples <- aperm(theta_samples, c(1, 4, 2, 3))
+dim(theta_samples) <- c((2501 * 20), 5, 40)
+theta_samples <- theta_samples[seq(1, length(theta_samples[, 1, 1]), 10), , ]
 
-#sampled$samples$alpha[,,sampled$samples$stage=="sample"]
+# sampled$samples$alpha[,,sampled$samples$stage=="sample"]
 # store the mu - phi
-phi_samples <- phi[,,burnin:5000]
-phi_samples <- aperm(phi_samples, c(1,3,2))
-dim(phi_samples)<- c(((n_iter+1)*n.chains),n_randeffect*2)
-phi_samples <- t(phi_samples[seq(1, length(phi_samples[,1]), 10),]) #thinning
+phi_samples <- phi[, , burnin:5000]
+phi_samples <- aperm(phi_samples, c(1, 3, 2))
+dim(phi_samples) <- c(((n_iter + 1) * n.chains), n_randeffect * 2)
+phi_samples <- t(phi_samples[seq(1, length(phi_samples[, 1]), 10), ]) # thinning
 
 
-n_iter <- length(phi_samples[1,])
-n.params<- n_randeffect*2+n_randeffect
-all_samples=array(dim=c(n_subjects,n.params,n_iter))
-mu_tilde=array(dim = c(n_subjects,n.params))
-sigma_tilde=array(dim = c(n_subjects,n.params,n.params))
+n_iter <- length(phi_samples[1, ])
+n.params <- n_randeffect * 2 + n_randeffect
+all_samples <- array(dim = c(n_subjects, n.params, n_iter))
+mu_tilde <- array(dim = c(n_subjects, n.params))
+sigma_tilde <- array(dim = c(n_subjects, n.params, n.params))
 
-#This is pretty much the same. Get theta and phi together for each subject and 
-#then get the mu_tilde as the means and the sigma as the covariance of those 21 means
-for (j in 1:n_subjects){
-  theta_new <- theta_samples[,,j]
-  all_samples[j,,] = rbind(t(theta_new),phi_samples[,])
+# This is pretty much the same. Get theta and phi together for each subject and
+# then get the mu_tilde as the means and the sigma as the covariance of those 21 means
+for (j in 1:n_subjects) {
+  theta_new <- theta_samples[, , j]
+  all_samples[j, , ] <- rbind(t(theta_new), phi_samples[, ])
   # calculate the mean for re, mu and sigma
-  mu_tilde[j,] =apply(all_samples[j,,],1,mean)
+  mu_tilde[j, ] <- apply(all_samples[j, , ], 1, mean)
   # calculate the covariance matrix for random effects, mu and sigma
-  sigma_tilde[j,,] = cov(t(all_samples[j,,]))
+  sigma_tilde[j, , ] <- cov(t(all_samples[j, , ]))
 }
 
 
 #### this is the same, just bind theta and phi together in the right order?
 X <- t(phi_samples)
 # do k=2, for a mixture of 2 gaussians (Davids suggestions, could be between 1-5 really)
-k = 2 #number of dists
+k <- 2 # number of dists
 
-#mvnormalmixEM is a weak point - function can fail. needs a note or output to show if it doesn't work. Should restart if it fails
-mix = NULL
-while(is.null(mix)) {
-  tryCatch(mix<-mixtools::mvnormalmixEM(X,k=k, maxit = 5000),error=function(e){
-  },finally={})
+# mvnormalmixEM is a weak point - function can fail. needs a note or output to show if it doesn't work. Should restart if it fails
+mix <- NULL
+while (is.null(mix)) {
+  tryCatch(mix <- mixtools::mvnormalmixEM(X, k = k, maxit = 5000), error = function(e) {
+  }, finally = {})
 }
 
 ###### generate the proposal parameters from the mix of importance samples  #####
 
 # use the weight to get samples for n1. n2 = samples-n1 (i.e 9000 and 1000)
-n1=rbinom(n=1, size=IS_samples,prob=max(mix$lambda))
-n1=pmax(n1,2)
-n1=pmin(n1,IS_samples-2)
-n2=IS_samples-n1
+n1 <- rbinom(n = 1, size = IS_samples, prob = max(mix$lambda))
+n1 <- pmax(n1, 2)
+n1 <- pmin(n1, IS_samples - 2)
+n2 <- IS_samples - n1
 
 # generates the 10,000 IS proposals given the mix
-proposals1=rmvnorm(n1,mix$mu[[1]],mix$sigma[[1]])
-proposals2=rmvnorm(n2,mix$mu[[2]],mix$sigma[[2]])
-prop_theta=rbind(proposals1,proposals2)
+proposals1 <- rmvnorm(n1, mix$mu[[1]], mix$sigma[[1]])
+proposals2 <- rmvnorm(n2, mix$mu[[2]], mix$sigma[[2]])
+prop_theta <- rbind(proposals1, proposals2)
 
 
 
@@ -101,7 +101,7 @@ prop_theta=rbind(proposals1,proposals2)
 
 ##### make it work
 
-#makes an array to store the IS samples
+# makes an array to store the IS samples
 tmp <- array(dim = c(IS_samples))
 
 # Autofill the par.names for log.dens.like
@@ -111,7 +111,7 @@ adj.log.dens.like <- function(x, data) {
 
 data <- do.call(rbind, data)
 
-#do the sampling
+# do the sampling
 if (cpus > 1) {
   tmp <- mclapply(
     X = 1:IS_samples,
@@ -151,28 +151,28 @@ if (cpus > 1) {
       n.params = n.params,
       par.names = par.names,
       ll_func = adj.log.dens.like
-      )
+    )
   }
 }
 
 save.image("IS2_de_bs.Rdata")
 # get the ML value
 finished <- tmp
-tmp<-unlist(tmp)
+tmp <- unlist(tmp)
 max.lw <- max(tmp)
-mean.centred.lw <- mean(exp(tmp-max.lw)) #takes off the max and gets mean (avoids infs)
-lw <-log(mean.centred.lw)+max.lw #puts max back on to get the lw
+mean.centred.lw <- mean(exp(tmp - max.lw)) # takes off the max and gets mean (avoids infs)
+lw <- log(mean.centred.lw) + max.lw # puts max back on to get the lw
 
 
 
 ##### bootstrapping for SE ######
-bootstrap = 10000
-log_marglik_boot= array(dim = bootstrap)
-for (i in 1:bootstrap){
-  log_weight_boot = sample(tmp, IS_samples, replace = TRUE) #resample with replacement from the lw
+bootstrap <- 10000
+log_marglik_boot <- array(dim = bootstrap)
+for (i in 1:bootstrap) {
+  log_weight_boot <- sample(tmp, IS_samples, replace = TRUE) # resample with replacement from the lw
   max.boot <- max(log_weight_boot)
-  centred.boot <- mean(exp(log_weight_boot-max.boot)) #takes off the max and gets mean (avoids infs)
-  log_marglik_boot[i] <-log(centred.boot)+max.boot #puts max back on 
+  centred.boot <- mean(exp(log_weight_boot - max.boot)) # takes off the max and gets mean (avoids infs)
+  log_marglik_boot[i] <- log(centred.boot) + max.boot # puts max back on
 }
 var(log_marglik_boot)
 
--- IS2_pmwg.R
+++ IS2_pmwg.R
@@ -1,7 +1,7 @@
 #######       IS2 Psuedo Code       #########
 
-## set up environment and packages 
-rm(list=ls())
+## set up environment and packages
+rm(list = ls())
 library(mvtnorm)
 library(MCMCpack)
 library(rtdists)
@@ -12,76 +12,76 @@ library(parallel)
 devtools::load_all()
 load("forstmann_long.Rdata")
 
-cpus = 1
+cpus <- 1
 ###### set up variables #####
 # number of particles, samples, subjects, random effects etc
-n_randeffect=sampled$n_pars
-n_subjects = sampled$n_subjects
-n_iter = length(sampled$samples$stage[sampled$samples$stage=="sample"])
-length_draws = sampled$samples$idx #length of the full transformed random effect vector and/or parameter vector
-IS_samples = 100 #number of importance samples
-n_particles = 10 #number of particles
-v_alpha = 2  #?
-pars = sampled$pars
+n_randeffect <- sampled$n_pars
+n_subjects <- sampled$n_subjects
+n_iter <- length(sampled$samples$stage[sampled$samples$stage == "sample"])
+length_draws <- sampled$samples$idx # length of the full transformed random effect vector and/or parameter vector
+IS_samples <- 100 # number of importance samples
+n_particles <- 10 # number of particles
+v_alpha <- 2 # ?
+pars <- sampled$pars
 
 
 # grab the sampled stage of PMwG
 # store the random effects
-alpha <- sampled$samples$alpha[,,sampled$samples$stage=="sample"]
+alpha <- sampled$samples$alpha[, , sampled$samples$stage == "sample"]
 # store the mu
-theta <- sampled$samples$theta_mu[,sampled$samples$stage=="sample"]
+theta <- sampled$samples$theta_mu[, sampled$samples$stage == "sample"]
 # store the cholesky transformed sigma
-sig <- sampled$samples$theta_sig[,,sampled$samples$stage=="sample"]
-# the a-hlaf is used in  calculating the Huang and Wand (2013) prior. 
+sig <- sampled$samples$theta_sig[, , sampled$samples$stage == "sample"]
+# the a-hlaf is used in  calculating the Huang and Wand (2013) prior.
 # The a is a random sample from inv gamma which weights the inv wishart. The mix of inverse wisharts is the prior on the correlation matrix
-a_half <- log(sampled$samples$a_half[,sampled$samples$stage=="sample"])
+a_half <- log(sampled$samples$a_half[, sampled$samples$stage == "sample"])
 
 
 
 
-#unwound sigma
-pts2.unwound = apply(sig,3,unwind)
-n.params<- nrow(pts2.unwound)+n_randeffect+n_randeffect
-all_samples=array(dim=c(n_subjects,n.params,n_iter))
-mu_tilde=array(dim = c(n_subjects,n.params))
-sigma_tilde=array(dim = c(n_subjects,n.params,n.params))
+# unwound sigma
+pts2.unwound <- apply(sig, 3, unwind)
+n.params <- nrow(pts2.unwound) + n_randeffect + n_randeffect
+all_samples <- array(dim = c(n_subjects, n.params, n_iter))
+mu_tilde <- array(dim = c(n_subjects, n.params))
+sigma_tilde <- array(dim = c(n_subjects, n.params, n.params))
 
 
-for (j in 1:n_subjects){
-  all_samples[j,,] = rbind(alpha[,j,],theta[,],pts2.unwound[,])
+for (j in 1:n_subjects) {
+  all_samples[j, , ] <- rbind(alpha[, j, ], theta[, ], pts2.unwound[, ])
   # calculate the mean for re, mu and sigma
-  mu_tilde[j,] =apply(all_samples[j,,],1,mean)
+  mu_tilde[j, ] <- apply(all_samples[j, , ], 1, mean)
   # calculate the covariance matrix for random effects, mu and sigma
-  sigma_tilde[j,,] = cov(t(all_samples[j,,]))
+  sigma_tilde[j, , ] <- cov(t(all_samples[j, , ]))
 }
 
 
 
-X <- cbind(t(theta),t(pts2.unwound),t(a_half)) 
+X <- cbind(t(theta), t(pts2.unwound), t(a_half))
 
 # do k=2, for a mixture of 2 gaussians (Davids suggestions, could be between 1-5 really)
-k = 2 #number of dists
+k <- 2 # number of dists
 
-#mvnormalmixEM is a weak point - function can fail. needs a note or output to show if it doesn't work. Should restart if it fails
-mix = NULL
+# mvnormalmixEM is a weak point - function can fail. needs a note or output to show if it doesn't work. Should restart if it fails
+mix <- NULL
 
-while(is.null(mix)) {
-  tryCatch(mix<-mvnormalmixEM(X,k=k, maxit = 5000),error=function(e){
-  },finally={})
+while (is.null(mix)) {
+  tryCatch(mix <- mvnormalmixEM(X, k = k, maxit = 5000), error = function(e) {
+  }, finally = {})
 }
 
 ###### generate the proposal parameters from the mix of importance samples  #####
 
 # use the weight to get samples for n1. n2 = samples-n1 (i.e 9000 and 1000)
-n1=rbinom(n=1, size=IS_samples,prob=max(mix$lambda))
-n1=pmax(n1,2)
-n1=pmin(n1,IS_samples-2)
-n2=IS_samples-n1
+n1 <- rbinom(n = 1, size = IS_samples, prob = max(mix$lambda))
+n1 <- pmax(n1, 2)
+n1 <- pmin(n1, IS_samples - 2)
+n2 <- IS_samples - n1
 
 # generates the 10,000 IS proposals given the mix
-proposals1=rmvnorm(n1,mix$mu[[1]],mix$sigma[[1]])
-proposals2=rmvnorm(n2,mix$mu[[2]],mix$sigma[[2]])
-prop_theta=rbind(proposals1,proposals2)
+proposals1 <- rmvnorm(n1, mix$mu[[1]], mix$sigma[[1]])
+proposals2 <- rmvnorm(n2, mix$mu[[2]], mix$sigma[[2]])
+prop_theta <- rbind(proposals1, proposals2)
 
 
 
@@ -104,11 +104,11 @@ prop_theta=rbind(proposals1,proposals2)
 
 ##### make it work
 
-#makes an array to store the IS samples
-tmp<-array(dim=c(IS_samples))
+# makes an array to store the IS samples
+tmp <- array(dim = c(IS_samples))
 
 
-#do the sampling
+# do the sampling
 if (cpus > 1) {
   tmp <- mclapply(
     X = 1:IS_samples,
@@ -129,7 +129,7 @@ if (cpus > 1) {
     par.names = sampled$par_names,
     ll_func = sampled$ll_func
   )
-} else{
+} else {
   for (i in 1:IS_samples) {
     cat(i)
     tmp[i] <- compute_lw(
@@ -155,24 +155,23 @@ if (cpus > 1) {
 
 # get the ML value
 finished <- tmp
-tmp<-unlist(tmp)
+tmp <- unlist(tmp)
 max.lw <- max(tmp)
-mean.centred.lw <- mean(exp(tmp-max.lw)) #takes off the max and gets mean (avoids infs)
-lw <-log(mean.centred.lw)+max.lw #puts max back on to get the lw
+mean.centred.lw <- mean(exp(tmp - max.lw)) # takes off the max and gets mean (avoids infs)
+lw <- log(mean.centred.lw) + max.lw # puts max back on to get the lw
 
 
 
 ##### bootstrapping for SE ######
-bootstrap = 10000
-log_marglik_boot= array(dim = bootstrap)
-for (i in 1:bootstrap){
-  log_weight_boot = sample(tmp, IS_samples, replace = TRUE) #resample with replacement from the lw
+bootstrap <- 10000
+log_marglik_boot <- array(dim = bootstrap)
+for (i in 1:bootstrap) {
+  log_weight_boot <- sample(tmp, IS_samples, replace = TRUE) # resample with replacement from the lw
   max.boot <- max(log_weight_boot)
-  centred.boot <- mean(exp(log_weight_boot-max.boot)) #takes off the max and gets mean (avoids infs)
-  log_marglik_boot[i] <-log(centred.boot)+max.boot #puts max back on 
+  centred.boot <- mean(exp(log_weight_boot - max.boot)) # takes off the max and gets mean (avoids infs)
+  log_marglik_boot[i] <- log(centred.boot) + max.boot # puts max back on
 }
 var(log_marglik_boot)
 
 
 save.image("IS2_v2.Rdata")
-
