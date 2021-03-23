e62a98190e2a54a7fcd478a975bcdb47e474d443
Gavin Cooper <phd@gavincooper.net>
2021-03-23 22:58:48 +1100

    Var renames as lintr fixes for DE script

--- IS2_de_b.R
+++ IS2_de_b.R
@@ -20,11 +20,11 @@ cpus <- 1
 n_randeffect <- length(theta[1, , 1, 1])
 n_subjects <- length(theta[1, 1, , 1])
 n_iter <- length(theta[1, 1, 1, ]) - burnin
-# length_draws = sampled$samples$idx #length of the full transformed random effect vector and/or parameter vector
-IS_samples <- 100 # number of importance samples
+#length of the full transformed random effect vector and/or parameter vector
+importance_samples <- 100 # number of importance samples
 n_particles <- 100 # number of particles
-par.names <- names(theta[1, , 1, 1])
-hpar.names <- names(phi[1, , 1])
+par_names <- names(theta[1, , 1, 1])
+hpar_names <- names(phi[1, , 1])
 
 
 # grab the sampled stage of PMwG
@@ -34,7 +34,6 @@ theta_samples <- aperm(theta_samples, c(1, 4, 2, 3))
 dim(theta_samples) <- c((2501 * 20), 5, 40)
 theta_samples <- theta_samples[seq(1, length(theta_samples[, 1, 1]), 10), , ]
 
-# sampled$samples$alpha[,,sampled$samples$stage=="sample"]
 # store the mu - phi
 phi_samples <- phi[, , burnin:5000]
 phi_samples <- aperm(phi_samples, c(1, 3, 2))
@@ -43,13 +42,14 @@ phi_samples <- t(phi_samples[seq(1, length(phi_samples[, 1]), 10), ]) # thinning
 
 
 n_iter <- length(phi_samples[1, ])
-n.params <- n_randeffect * 2 + n_randeffect
-all_samples <- array(dim = c(n_subjects, n.params, n_iter))
-mu_tilde <- array(dim = c(n_subjects, n.params))
-sigma_tilde <- array(dim = c(n_subjects, n.params, n.params))
+n_params <- n_randeffect * 2 + n_randeffect
+all_samples <- array(dim = c(n_subjects, n_params, n_iter))
+mu_tilde <- array(dim = c(n_subjects, n_params))
+sigma_tilde <- array(dim = c(n_subjects, n_params, n_params))
 
 # This is pretty much the same. Get theta and phi together for each subject and
-# then get the mu_tilde as the means and the sigma as the covariance of those 21 means
+# then get the mu_tilde as the means and the sigma as the covariance of those
+# 21 means
 for (j in 1:n_subjects) {
   theta_new <- theta_samples[, , j]
   all_samples[j, , ] <- rbind(t(theta_new), phi_samples[, ])
@@ -61,52 +61,38 @@ for (j in 1:n_subjects) {
 
 
 #### this is the same, just bind theta and phi together in the right order?
-X <- t(phi_samples)
-# do k=2, for a mixture of 2 gaussians (Davids suggestions, could be between 1-5 really)
+parvector <- t(phi_samples)
+# do k=2, for a mixture of 2 gaussians (Davids suggestions, could be between
+# 1-5 really)
 k <- 2 # number of dists
 
-# mvnormalmixEM is a weak point - function can fail. needs a note or output to show if it doesn't work. Should restart if it fails
+# mvnormalmixEM is a weak point - function can fail. needs a note or output to
+# show if it doesn't work. Should restart if it fails
 mix <- NULL
 while (is.null(mix)) {
-  tryCatch(mix <- mixtools::mvnormalmixEM(X, k = k, maxit = 5000), error = function(e) {
-  }, finally = {})
+  tryCatch(mix <- mixtools::mvnormalmixEM(parvector, k = k, maxit = 5000))
 }
 
-###### generate the proposal parameters from the mix of importance samples  #####
+#### generate the proposal parameters from the mix of importance samples  ####
 
 # use the weight to get samples for n1. n2 = samples-n1 (i.e 9000 and 1000)
-n1 <- rbinom(n = 1, size = IS_samples, prob = max(mix$lambda))
+n1 <- rbinom(n = 1, size = importance_samples, prob = max(mix$lambda))
 n1 <- pmax(n1, 2)
-n1 <- pmin(n1, IS_samples - 2)
-n2 <- IS_samples - n1
+n1 <- pmin(n1, importance_samples - 2)
+n2 <- importance_samples - n1
 
 # generates the 10,000 IS proposals given the mix
 proposals1 <- rmvnorm(n1, mix$mu[[1]], mix$sigma[[1]])
 proposals2 <- rmvnorm(n2, mix$mu[[2]], mix$sigma[[2]])
 prop_theta <- rbind(proposals1, proposals2)
 
-
-
-
-### main functions
-
-# get_params = function(parameters, n_randeffect){
-#   param.theta.mu <- parameters[1:n_randeffect]
-#   param.theta.sig.unwound <- parameters[(n_randeffect+1):(length(parameters)-n_randeffect)] ##scott would like it to ask for n(unwind)
-#   param.theta.sig2 <- unwind(param.theta.sig.unwound, reverse = TRUE)
-#   param.a <- exp(parameters[((length(parameters)-n_randeffect)+1):(length(parameters))])
-# }
-
-
-
-##### make it work
-
 # makes an array to store the IS samples
-tmp <- array(dim = c(IS_samples))
+tmp <- array(dim = c(importance_samples))
 
 # Autofill the par.names for log.dens.like
-adj.log.dens.like <- function(x, data) {
-  log.dens.like(x, data, par.names = par.names)
+# log.dens.like comes from load("b_fit.RData")
+adj_log_dens_like <- function(x, data) {
+  log.dens.like(x, data, par.names = par_names)
 }
 
 data <- do.call(rbind, data)
@@ -114,7 +100,7 @@ data <- do.call(rbind, data)
 # do the sampling
 if (cpus > 1) {
   tmp <- mclapply(
-    X = 1:IS_samples,
+    X = 1:importance_samples,
     mc.cores = cpus,
     FUN = compute_lw,
     prop_theta = prop_theta,
@@ -128,12 +114,12 @@ if (cpus > 1) {
     prior = prior,
     group_dist = group_dist_de,
     mix = mix,
-    n.params = n.params,
-    par.names = par.names,
-    ll_func = adj.log.dens.like
+    n_params = n_params,
+    par_names = par_names,
+    ll_func = adj_log_dens_like
   )
 } else {
-  for (i in 1:IS_samples) {
+  for (i in 1:importance_samples) {
     cat(i, sep = "_")
     tmp[i] <- compute_lw(
       prop_theta,
@@ -148,9 +134,9 @@ if (cpus > 1) {
       prior = prior,
       group_dist = group_dist_de,
       mix = mix,
-      n.params = n.params,
-      par.names = par.names,
-      ll_func = adj.log.dens.like
+      n_params = n_params,
+      par_names = par_names,
+      ll_func = adj_log_dens_like
     )
   }
 }
@@ -159,9 +145,10 @@ save.image("IS2_de_bs.Rdata")
 # get the ML value
 finished <- tmp
 tmp <- unlist(tmp)
-max.lw <- max(tmp)
-mean.centred.lw <- mean(exp(tmp - max.lw)) # takes off the max and gets mean (avoids infs)
-lw <- log(mean.centred.lw) + max.lw # puts max back on to get the lw
+max_lw <- max(tmp)
+# takes off the max and gets mean (avoids infs)
+mean_centred_lw <- mean(exp(tmp - max_lw))
+lw <- log(mean_centred_lw) + max_lw # puts max back on to get the lw
 
 
 
@@ -169,10 +156,12 @@ lw <- log(mean.centred.lw) + max.lw # puts max back on to get the lw
 bootstrap <- 10000
 log_marglik_boot <- array(dim = bootstrap)
 for (i in 1:bootstrap) {
-  log_weight_boot <- sample(tmp, IS_samples, replace = TRUE) # resample with replacement from the lw
-  max.boot <- max(log_weight_boot)
-  centred.boot <- mean(exp(log_weight_boot - max.boot)) # takes off the max and gets mean (avoids infs)
-  log_marglik_boot[i] <- log(centred.boot) + max.boot # puts max back on
+  # resample with replacement from the lw
+  log_weight_boot <- sample(tmp, importance_samples, replace = TRUE)
+  max_boot <- max(log_weight_boot)
+  # takes off the max and gets mean (avoids infs)
+  centred_boot <- mean(exp(log_weight_boot - max_boot))
+  log_marglik_boot[i] <- log(centred_boot) + max_boot # puts max back on
 }
 var(log_marglik_boot)
 
